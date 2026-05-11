086c245993084f99b0d4d57a3a077cd430a3b397
Abdullah <179810492+Abdullah-Jabbar786@users.noreply.github.com>
2026-05-11 11:35:01 +0500

    Update test_mutex.sh

--- test_mutex.sh
+++ test_mutex.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-# STRESS TEST: Thread Pool, Mutex Locks, and IPC Sockets
+#STRESS TEST: Thread Pool, Mutex Locks, and IPC Sockets.
 
 echo -e "\e[1;36m[+] Initializing OS-Level Concurrency Stress Test...\e[0m"
 sleep 1
 echo -e "\e[1;33m[+] Spawning 50 concurrent child processes to flood the server...\e[0m"
 
-# The exam now has 10 questions. Because the server randomly shuffles 
-# We just send 10 dummy answers 
-# To force all clients to finish the exam as fast as possible.
+#The exam now has 10 questions. Because the server randomly shuffles.
 ANSWERS="test\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n"
 
 for i in {1..30}
 do
-   # The '&' symbol forks a background process to run the client
-   # We use "$ANSWERS" in quotes so bash formats the newlines correctly!
+   #The '&' symbol forks a background process to run the client.
+   #We use "$ANSWERS" in quotes so bash formats the newlines correctly!
    (echo -e "$ANSWERS" | ./client > /dev/null 2>&1) &
    
-   # (since our listen() backlog in server.c is set to MAX_CLIENTS = 10)
+   #(since our listen() backlog in server.c is set to MAX_CLIENTS = 10)
    sleep 0.1
 done
 
